364d0e2972c6f4b71c1d3f29d5e4f2895c3b0fb8
Pat McCue <psmccue@gmail.com>
2023-12-11 14:20:36 -0700

    beginning of chapter 14 for Next.js dashboard tutorial

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -103,6 +103,7 @@ export async function updateInvoice(
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
   export async function deleteInvoice(id: string) {
     // throw new Error('Failed to Delete Invoice');
    
